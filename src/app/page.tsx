ed4830066ed7d30b560967b66f94e824f5a85778
kophe <lorenzo.bosi.pro@gmail.com>
2024-11-20 21:55:32 +0100

    Page without under content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,313 @@
+'use client';
+
+import clsx from "clsx";
+import { delay, motion } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Page() {
+
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <motion.main className="flex items-center justify-center w-full h-screen relative">
+      {/* <div className="w-4 h-4 bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20"></div> */}
+
+      <section className="flex items-center justify-center w-full h-screen absolute top-0 left-0">
+
+        <div className="w-full h-screen bg-white flex items-center justify-start gap-4 flex-col p-4">
+
+          {/* Up */}
+          <div className="w-full h-[10.5vh] relative flex justify-center items-center gap-4">
+            <motion.div
+              className="w-full h-full bg-primary-300 rounded-xl"
+              // initial={{ opacity: 0.5, scale: 0, y: 70 }}
+              // animate={{ opacity: 1, scale: 1, y: 0 }}
+              // transition={{ duration: 0.5,
+              //   delay: 1  }}
+              initial={{ opacity: 0.5, scale: 0, y: 70 }}
+              animate={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{ 
+                duration: 1.5,
+                delay: 1.8,
+                ease: "circOut",
+                // type: "spring",
+                bounce: 0,  
+              }}
+            >
+
+            </motion.div>
+          </div>
+
+          {/* Down */}
+          <div className="w-full h-[85vh] relative flex  gap-4 ">
+
+            {/* Left */}
+            <div className="w-4/6 h-full  gap-4 flex flex-col justify-end">
+
+              {/* Up Left */}
+              <div className="h-3/5 gap-4 flex flex-row">
+
+                {/* Title */}
+                <motion.div 
+                  className="w-7/12 h-full flex justify-end items-end"
+                  initial={{
+                    width: "58.3%",
+                  }}
+                  animate={{
+                    width: "66.6%",
+                  }}
+                  transition={{ 
+                    duration: 2,
+                    delay: 1.3,
+                    ease: "circOut",
+                    // type: "tween",
+                      bounce: 0,    
+                  }}
+                >
+                  <motion.div
+                    className="w-full h-full bg-primary-300 rounded-xl"
+                    initial={{ opacity: 0.6, scale: 0, x: 470, y: 60 }}
+                    animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                    transition={{ 
+                      duration: 1.45,
+                      delay: 1.85,
+                      // duration: 1.3,
+                      // delay: 2,
+                      ease: "circOut",
+                      // type: "spring",
+                      bounce: 0,  
+                    }}
+                  >
+
+                  </motion.div>
+                </motion.div>
+
+                {/* Profil Picture */}
+                <motion.div 
+                  className="bg-primary-300 rounded-xl w-2/6 h-full relative z-20"
+                  initial={{ scale: 1.2,}}
+                  animate={{ scale: 1,}}
+                  transition={{ 
+                    duration: 0.8,
+                    delay: 0.2,
+                    ease: "easeIn",
+                    type: "spring",
+                    // bounce: 0,  
+                   }}
+
+                >
+                  <Image
+                    src="/img/asiat.png"
+                    alt="asiat"
+                    fill
+                    className="rounded-xl object-cover object-center"
+                  />
+                </motion.div>
+
+              </div>
+
+              {/* Down Left */}
+              <motion.div 
+                className="h-1/4 gap-4 flex flex-row"
+                initial={{
+                  height: "25%",
+                }}
+                animate={{
+                  height: "40%",
+                }}
+                transition={{ 
+                  duration: 2,
+                  delay: 1.3,
+                  ease: "circOut",
+                  // type: "spring",
+                  bounce: 0,   
+                }}                
+              >
+
+                {/* Description */}
+                <div className="rounded-xl w-3/6 flex items-start justify-start">
+                  <motion.div 
+                    className="bg-primary-300 rounded-xl w-full h-full"
+                    initial={{ opacity: 0.5, scale: 0, x: 470, y: -160 }}
+                    animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                    transition={{ 
+                      duration: 1.35,
+                      delay: 1.95, 
+                      // duration: 1,
+                      // delay: 2.3,
+                      ease: "circOut",
+                      // type: "spring",
+                      bounce: 0,  
+                    }}
+                  >
+
+                  </motion.div>
+                </div>
+
+                {/* Contact */}
+                <div className="rounded-xl w-3/6">
+                  <motion.div 
+                    className="bg-primary-400 rounded-xl w-full h-full"
+                    initial={{ opacity: 0.5, scale: 0, x: 70, y: -160 }}
+                    animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                    transition={{
+                      duration: 1.3,
+                      delay: 2,  
+                      // duration: 0.8,
+                      // delay: 2.5,
+                      ease: "circOut",
+                      // type: "spring",
+                      bounce: 0,  
+                    }}
+                  ></motion.div>
+                </div>
+              
+              </motion.div>
+
+            </div>
+
+            {/* Right */}
+            <div className="w-2/6 h-full  gap-4 flex flex-col">
+
+              {/* Projects */}
+              <div className="rounded-xl w-full h-5/6">
+                <motion.div 
+                  className="bg-primary-300 h-full rounded-xl w-full"
+                  initial={{ opacity: 0.7, scale: 0, x: -470, y: -160 }}
+                  animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                  transition={{ 
+                    duration: 1.4,
+                    delay: 1.9,
+                    // duration: 1.25,
+                    // delay: 2.05,
+                    ease: "circOut",
+                    // type: "spring",
+                    bounce: 0,  
+                  }}
+                >
+
+                </motion.div>
+              </div>
+
+              {/* Footer */} 
+              <div className="rounded-xl w-full h-1/6">
+                <motion.div 
+                  className="bg-primary-300 rounded-xl w-full h-full"
+                  initial={{ opacity: 0.5, scale: 0, x: 0, y: -150 }}
+                  animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+                  transition={{
+                    duration: 1.35,
+                    delay: 1.95, 
+                    // duration: 1.1,
+                    // delay: 2.2,
+                    ease: "circOut",
+                    // type: "spring",
+                    bounce: 0,  
+                  }}
+                ></motion.div>  
+              </div>
+
+            </div>
+
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+      </section>
+
+    </motion.main>
+
+
+
+    // <motion.main className="flex items-center justify-center w-full h-screen relative">
+    //   <div className="w-4 h-4 bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20"></div>
+
+    //   <section className="flex items-center justify-center w-full h-screen absolute top-0 left-0">
+
+    //     <div className="w-full h-screen bg-white grid gap-4 grid-cols-12 grid-rows-8 p-4">
+
+    //       {/* Nav */}
+    //       <div className=" col-span-full row-span-1 relative flex justify-center items-center">
+    //         <motion.div
+    //           className="w-full h-full bg-[#FADCD9] rounded-xl"
+    //           initial={{ opacity: 0.5, scale: 0, y: 70 }}
+    //           animate={{ opacity: 1, scale: 1, y: 0 }}
+    //           transition={{ duration: 0.5 }}
+    //         >
+
+    //         </motion.div>
+    //       </div>
+
+    //       {/* Title */}
+    //       <div className="col-span-5 row-span-4 flex justify-end items-end">
+    //         <motion.div
+    //           className="w-full h-full bg-[#FADCD9] rounded-xl"
+    //           initial={{ opacity: 0.5, scale: 0, x: 270, y: 60 }}
+    //           animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+    //           // initial={{ opacity: 0.5, scale: 0, x: 270, y: 60 }}
+    //           // animate={{ opacity: 1, scale: 1, x: 0, y: 0 }}
+    //           transition={{ duration: 0.5 }}
+    //         >
+
+    //         </motion.div>
+    //       </div>
+
+    //       {/* Profil Picture */}
+    //       <motion.div 
+    //         className="bg-[#FADCD9] rounded-xl col-span-3 row-span-4 relative"
+    //         initial={{ scale: 1.2, y: 70 }}
+    //         animate={{ scale: 1, y: 0 }}
+    //         transition={{ duration: 0.5 }}
+
+    //       >
+    //         <Image
+    //           src="/img/asiat.png"
+    //           alt="asiat"
+    //           layout="fill"
+    //           objectFit="cover"
+    //           objectPosition="right center"
+    //           className="rounded-xl"
+    //         />
+    //       </motion.div>
+
+    //       {/* Projects */}
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-6"></div>
+
+    //       {/* Description */}
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-3"></div>
+
+    //       {/* Contact */}
+    //       <div className="bg-[#F8AFA6] rounded-xl col-span-4 row-span-3"></div>
+
+    //       {/* Footer */} 
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-1"></div>
+    //     </div>
+
+    //   </section>
+
+    //   {/* <section className="flex items-center justify-center w-full h-screen absolute top-0 left-0">
+    //     <div className="w-full h-screen bg-white grid gap-4 grid-cols-12 grid-rows-8 p-4">
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-12 row-span-1"></div>
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-5 row-span-4"></div>
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-3 row-span-4 relative">
+    //         <Image
+    //           src="/img/asiat.png"
+    //           alt="asiat"
+    //           layout="fill"
+    //           objectFit="cover"
+    //           objectPosition="right center"
+    //           className="rounded-xl"
+    //         />
+    //       </div>
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-6"></div>
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-3"></div>
+    //       <div className="bg-[#F8AFA6] rounded-xl col-span-4 row-span-3"></div>
+    //       <div className="bg-[#FADCD9] rounded-xl col-span-4 row-span-1"></div>
+    //     </div>
+    //   </section> */}
+
+    // </motion.main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,40 @@ export default {
   ],
   theme: {
     extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+        'portrait': '9 / 16',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {  
+          DEFAULT: '#F8AFA6',  
+          50: '#FFFFFF',  
+          100: '#FFFFFF',  
+          200: '#FEF3F2',  
+          300: '#FBD1CC',  
+          400: '#F8AFA6',  
+          500: '#F48072',  
+          600: '#F0513E',  
+          700: '#E42912',  
+          800: '#B0200E',  
+          900: '#7C160A',  
+          950: '#621208'},
+        secondary: {
+          DEFAULT: '#3B82F6',  
+          50: '#EBF2FE',  
+          100: '#D7E6FD',  
+          200: '#B0CDFB',  
+          300: '#89B4FA',  
+          400: '#629BF8',  
+          500: '#3B82F6',  
+          600: '#0B61EE',  
+          700: '#084BB8',  
+          800: '#063583',  
+          900: '#041F4D',  
+          950: '#021532'
+        },
       },
     },
   },

5b07351a82b9e2b06dd45a9c26424f110c2ec700
kophe <lorenzo.bosi.pro@gmail.com>
2024-11-25 18:34:39 +0100

    FIXED ANIM IMG - *O*

--- src/app/image-animation/page.tsx
+++ src/app/image-animation/page.tsx
@@ -1,22 +1,7 @@
 'use client';
-
-import { useState, useEffect } from 'react';
-import Image from "next/image";
-import { motion, useScroll, useTransform } from "framer-motion";
-import { image } from 'framer-motion/client';
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 export default function Page() {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   // Tableau d'image
   const images = [
@@ -27,43 +12,40 @@ export default function Page() {
     "/img/img6.jpg",
     "/img/img1.jpg",
     "/img/img2.jpg",
-
   ];
 
   const { scrollYProgress } = useScroll()
-//   console.log(scrollYProgress.get());
-  
+  const smoothProgress  = useSpring(scrollYProgress, { stiffness: 100, damping: 30, restDelta: 0.001 })
+
+  const x = useTransform(smoothProgress , [0, 1], ["0%", "-50%"]);
+  const objectPositionX = useTransform(smoothProgress, [0, 1], ["0%", "50%"]);
 
   return (
     <main className='overflow-clip scroll-smooth'>
         <section className="flex items-center justify-start  w-fit h-screen bg-neutral-900 sticky top-0 z-10">
             <motion.div
-            className='flex items-center justify-start ml-60 space-x-10'
-            style={{
-                // transform: `translateX(-${scrollPercentage * 50}%)`,
-                transform: `translateX(-${scrollYProgress.get() * 50}%)`,
-                // scaleX: scrollYProgress,
-                // left
-            }}
+            className='flex items-center justify-start ml-60 space-x-10 progress-bar'
+            style={{ x }}
             >
                 {images.map((image, index) => (
                     <div className="relative h-96 aspect-10/13 overflow-hidden bg-black"  key={index}>
+
                         {/* Boucle sur le tableau image pour afficher les images */}
-                            <Image
+
+                            <motion.img
                             src={image}
                             alt={`Image ${index + 1}`}
-                            fill
-                            className="object-cover object-left"
-                            style={{
-                                transition: "transform 0.3s ease-in-out",
-                                objectPosition: `${scrollYProgress.get() * 100}% center`,
-                                }}
+                            className="object-cover h-full w-full"
+                            style={{ 
+                              objectPosition: objectPositionX, 
+                            }}
                             />
+
                     </div>
                 ))}
             </motion.div>
         </section>
-        <section className='h-[650vh]'></section>
+        <section className='h-[200vh]'></section>
     </main>
   );
 }
